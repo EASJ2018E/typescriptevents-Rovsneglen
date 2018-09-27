01349cd0f1bd0fc7d84885929cbe957b1b63e01b
Jesper Tim Damgaard Krøyer <jesp9030@edu.easj.dk>
2018-09-27 15:26:46 +0200

    Done

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,16 @@
-//Her skal du skrive din typescriptkode
+let buttonVare = document.getElementById('Tilføjvareknap');
+let inputVare = document.getElementById('NyVareInput') as HTMLInputElement;
+let inkøbsListe = document.getElementById('liste');
+let counter = 4;
+
+buttonVare.addEventListener("click", AddVare);
+function AddVare()
+{
+    let opret = document.createElement('li');
+    opret.setAttribute("id", counter+"");
+    let text = document.createTextNode(inputVare.value);
+    opret.appendChild(text);
+    inkøbsListe.appendChild(opret);
+    counter++;
+    console.log(opret);
+}
